c59df27a1a93c3e9440b630b8700a10510c159a5
AsararAhmed <connect.asarar@gmail.com>
2025-04-14 09:04:30 +0530

    Undirectional flow of data

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,48 +1,83 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 export default function ExpenseForm({ setExpenses }) {
+
+  /*Unidirectional Data flow In React 
+  - UI cannot upadte data but data can update UI, so we  can only chnge data using state varaibles 
+  - React block the data upadte via UI if value attribute is aplliied , we need to use onChange() with it to upadte the data
+  */
+  const [expense, setExpense] = useState({
+    title: '',
+    category: '',
+    amount: '',
+  })
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    const expense = { ...getFormData(e.target), id: crypto.randomUUID() } 
-
-    setExpenses((prevState) => [...prevState, expense])
-    e.target.reset()
-  }
-
-  const getFormData = (form) => {
-    const formData = new FormData(form)
-    const data = {}
-    for (const [key, value] of formData.entries()) {
-      data[key] = value
-    }
-    console.log(data)
-    return data
+    setExpenses((prevState) => [
+      ...prevState,
+      { ...expense, id: crypto.randomUUID() },
+    ])
+    setExpense({
+      title: '',
+      category: '',
+      amount: '',
+    })
   }
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
-    <div className="input-container">
-      <label htmlFor="title">Title</label>
-      <input id="title" name="title"/>
-    </div>
-    <div className="input-container">
-      <label htmlFor="category">Category</label>
-      {/* <input id="category" /> */}
-      <select id="category"name="category">
-            <option value="">Select Category</option>
-            <option value="grocery">Grocery</option>
-            <option value="clothes">Clothes</option>
-            <option value="bills">Bills</option>
-            <option value="education">Education</option>
-            <option value="medicine">Medicine</option>
-          </select>
-    </div>
-    <div className="input-container">
-      <label htmlFor="amount">Amount</label>
-      <input id="amount" name="amount" />
-    </div>
-    <button className="add-btn">Add</button>
-  </form>
-    
+      <div className="input-container">
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          value={expense.title}
+          onChange={(e) =>
+            setExpense((prevState) => ({ ...prevState, title: e.target.value }))
+          }
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="category">Category</label>
+        <select
+          id="category"
+          name="category"
+          value={expense.category}
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              category: e.target.value,
+            }))
+          }
+        >
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="Grocery">Grocery</option>
+          <option value="Clothes">Clothes</option>
+          <option value="Bills">Bills</option>
+          <option value="Education">Education</option>
+          <option value="Medicine">Medicine</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor="amount">Amount</label>
+        <input
+          id="amount"
+          name="amount"
+          value={expense.amount}
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              amount: e.target.value,
+            }))
+          }
+        />
+      </div>
+      <button className="add-btn">Add</button>
+    </form>
   )
 }
+  
